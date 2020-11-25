b2556903856354c84416b95eb0b4eea37a236eb5
AndresMestra <amestraavila32@correo.unicordoba.edu.co>
2020-11-24 19:27:39 -0500

    Correcciones

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 
-//administrador de salas
-var {} = require("./bin/Rooms")
+//Sockets
+io.sockets.on('connect', (socket) => {
+    console.log(`Un nuevo usuario con id ${socket.id} se ha conectado`)
+
+    var msg = `El usuario con id ${socket.id} se ha conectado`
+
+    socket.emit('connect', msg);
+
+    socket.on('setPing', () => { // se activa en el servidor el evento on con nombre setPing enviado desde el cliente
+        socket.emit('setPong', 'Esto es un pong'); //enviamos una respuesta al cliente con un nuevo nombre
+    });
+})
 
 //cargar la plantilla html del juego
 app.get('/', (req, res) => {
